1578336409413f69b12be690d8debcfbb04f5a49
Santi G <24705975+santiagoa58@users.noreply.github.com>
2023-11-23 06:30:34 +0000

    Enable model CPU offload for base and refiner models

--- app/image-gen/image_generator.py
+++ app/image-gen/image_generator.py
@@ -9,7 +9,7 @@ app = FastAPI()
 base = DiffusionPipeline.from_pretrained(
     "stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, variant="fp16", use_safetensors=True
 )
-# base.to("cuda")
+base.enable_model_cpu_offload()
 refiner = DiffusionPipeline.from_pretrained(
     "stabilityai/stable-diffusion-xl-refiner-1.0",
     text_encoder_2=base.text_encoder_2,
@@ -18,7 +18,7 @@ refiner = DiffusionPipeline.from_pretrained(
     use_safetensors=True,
     variant="fp16",
 )
-# refiner.to("cuda")
+refiner.enable_model_cpu_offload()
 
 class GenImgRequest(BaseModel):
     # Text prompt with description of the things you want in the image to be generated
