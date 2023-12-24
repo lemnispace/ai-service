a2b8d92526b6c042e92e0adadb7f0c62df8061e2
Santi G. <24705975+santiagoa58@users.noreply.github.com>
2023-12-24 03:18:43 +0000

    increase lambda memory size

--- terraform/main.tf
+++ terraform/main.tf
@@ -59,6 +59,7 @@ resource "aws_lambda_function" "AIServiceFunction" {
   runtime          = "python3.11"
   source_code_hash = data.archive_file.AIServiceFunction.output_base64sha256
   timeout          = 30
+  memory_size      = 512
   environment {
     variables = {
       ALLOWED_ORIGINS        = var.allow_origins
